f5905eb71ba39eee07acefc2213c77ce5e7448b0
AiltonGalvao <ailton.galvao@semed.manaus.am.gov.br>
2023-12-21 20:35:05 -0400

    outraTarefaFeita

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Titulo from './Titulo';
 import BotaoLegal from './BotaoLegal';
+import Card from './componentes/Card';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <Titulo titulo= "Titulo"/>
-    <Titulo titulo= "Seja Bem-Vindo"/>
-    <BotaoLegal texto= "Me cadastrar" emoji ="😂"/>
-    <BotaoLegal texto= "Fazer Login" emoji = "🤳"/>
+    <Card alimento={'https://images.unsplash.com/photo-1586511925558-a4c6376fe65f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=60'}/>
+    <Card alimento= {'https://anamariabraga.globo.com/wp-content/uploads/2019/06/sucos-detox-desintoxicantes-1.jpg'}/>
   </div>
    
 );
